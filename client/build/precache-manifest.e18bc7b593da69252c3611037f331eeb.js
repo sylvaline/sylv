b42c7f027079b54435e0c6a9d3409cbe85e2947d
sylvaline <okekesylvaline80@gmail.com>
2021-04-21 16:02:33 +0100

    final corrections

---
+++ client/build/precache-manifest.e18bc7b593da69252c3611037f331eeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80050265541e5fb0d0c8f6c24f30e6a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d172362b62bc2b1e2403",
+    "url": "/static/css/main.9a78bc26.chunk.css"
+  },
+  {
+    "revision": "0090a65d7fc5d6668675",
+    "url": "/static/js/2.dac552ae.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.dac552ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d172362b62bc2b1e2403",
+    "url": "/static/js/main.52ac515e.chunk.js"
+  },
+  {
+    "revision": "ab1effefaeafa7021ca0",
+    "url": "/static/js/runtime-main.b7842403.js"
+  }
+]);
